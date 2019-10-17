8e2157d45c0715912cf50fbee90eac1dc108c26a
thangciu <53205940+thangciu@users.noreply.github.com>
2019-10-18 01:14:41 +0700

    update

--- js/controller/controller.js
+++ js/controller/controller.js
@@ -7,26 +7,24 @@ controller.initAuth = function () {
     function authStateChangedHandler(user) {
 
         if (user) {
-            if(user.email != 'taikhoantestci30@gamebatchu.io'){
+            if (user.email != 'taikhoantestci30@gamebatchu.io') {
                 model.authenticated(user)
                 view.showComponents('picklevel')
-            }else{
-               view.showComponents('picklevel')
+            } else {
+                view.showComponents('picklevel')
             }
-        } else{
+        } else {
             view.showComponents('welcome')
         }
     }
 }
 
-controller.register = async function(registerInfo)
-{
+controller.register = async function (registerInfo) {
     let user = registerInfo.user
     let password = registerInfo.password
     let displayName = registerInfo.displayName
-    
-    try
-    {
+
+    try {
         view.setText("register-error", "")
         view.setText("register-success", "")
         await firebase.auth().createUserWithEmailAndPassword(user, password)
@@ -34,140 +32,135 @@ controller.register = async function(registerInfo)
         firebase.auth().currentUser.updateProfile({
             displayName: displayName
         })
-        
+
     }
-    catch(err)
-    {
-       view.setText("register-error", "Tên người dùng đã tồn tại hoặc không hợp lệ")
+    catch (err) {
+        view.setText("register-error", "Tên người dùng đã tồn tại hoặc không hợp lệ")
     }
-  
+
 }
 
-controller.login = async function(loginInfo)
-{
-    
+controller.login = async function (loginInfo) {
+
     let user = loginInfo.user
     let password = loginInfo.password
-    
-    
+
+
     $("#btn-login-submit").attr("disable", true)
-    try
-    {
+    try {
         view.setText("login-error", "")
         let result = await firebase.auth().signInWithEmailAndPassword(user, password)
-        if(result.user)
-        {
+        if (result.user) {
             view.showComponents('picklevel')
         }
-    }catch(err)
-    {
-        
+    } catch (err) {
+
         view.setText("login-error", "Sai mật khẩu hoặc tài khoản không tồn tại!")
         $("#btn-login-submit").attr("disable")
     }
 }
 
 
-controller.lvg = async function(lv){
-   
-    let getData =  await firebase.firestore().collection('data').doc('data-ques').get()
+controller.lvg = async function (lv) {
+    $('#wrong-icon').hide()
+    $('#right-icon').hide()
+
+    let getData = await firebase.firestore().collection('data').doc('data-ques').get()
     //   $('#lv1').click = getlv(1)
     $('#level-text').html('Level ' + lv)
     $('#level-de-active').addClass('active')
     let idLv = '#lv' + lv
     $(idLv).addClass('active')
 
-        let data = await {
-            img: getData.data().de[lv - 1 ].img,
-            ans: getData.data().de[lv - 1].ans
-        }
-        console.log(data.ans)
-        console.log(data.img)
-        let inpStr = []
-        for(let i = 0; i < data.ans.length; i++){
-         inpStr += '<input class="answer" type="text" name="answer" maxlength="1">'
-         }
-    
-         $('.input-answer').html( inpStr)
-         $('#img-data').attr('src', data.img)
+    let data = await {
+        img: getData.data().de[lv - 1].img,
+        ans: getData.data().de[lv - 1].ans
+    }
+    let inpStr = []
+    for (let i = 0; i < data.ans.length; i++) {
+        inpStr += '<input class="answer" type="text" name="answer" maxlength="1">'
+    }
 
-         let ans = data.ans
+    $('.input-answer').html(inpStr)
+    $('#img-data').attr('src', data.img)
+
+    let ans = data.ans
     let str = ''
+
+    $('#level-text').css('color', 'white')
+    for(let i = 0; i < $('.answer').length; i++){
+        $('.answer')[i].setAttribute('id', 'a' + i)
+    }
+    
    
-
-    
-    $('#level-text').css('color','white')
-
-    $('#wrong-icon').hide()
-    $('#right-icon').hide()
-
-    $(function() {   
-        
+     $(function () {
         $('.answer').keyup(function () {
-            if (this.value.length == this.maxLength) {   
-                str += this.value
-              $(this).next('.answer').focus();
+            // console.log(str)
+            if (this.value.length == this.maxLength) {
+                // str += this.value
+                $(this).next('.answer').focus()
             }
-            console.log(str)
-            $('.answer').keyup(function(){     
-               if(this.value == ''){
-                $(this).attr('name', 'a')
-                // ind()
-                // function ind(){
-                    // let indx
-                    for(let i = 0; i < $('.answer').length; i++)
-                    {
-                        console.log($('.answer')[i].name)
-                        //  if( $('.answer')[i].value == 'a'){
-                        //     console.log(i)
-                        // }
-                    }
-                    // return indx
-                // }        
-               }
-              });
-
-            
-            if(str.length == ans.length){ 
-              let result = str.toLowerCase()
-            if(result != ans){
-                $('#level').addClass('wrong-answer')
-                $('.answer:first-child').focus()
-                $('#wrong-icon').show()
-
-                setTimeout(function(){
-                    $('#level').removeClass('wrong-answer')
-                }, 1000);
-                
-
-                let inputs = document.getElementsByClassName('answer')
-                for(let input of inputs){
-                   input.value = ''
-                   str = ''
-                }
-                $('#level-text').css('color','red')
-                $('#wrong-icon').css('color', 'red')
-
-            }else{
-                $('#wrong-icon').hide()
-                $('#right-icon').show()
-                $('#wrong-icon').css('color', 'rgb(56, 250, 56)')
-                $('#level-text').css('color','rgb(56, 250, 56)')
-
-                $('#level').addClass('right-answer')
-                for(let i = 0; i < $(('.answer')).length; i++){
-                    $('.answer')[i].disabled = true
-                }
-                point += 10
-                console.log(point)
-                let poi = document.getElementById('point')
-                poi.innerHTML = `
-                <span> Diem: ${point}</span>
-                `
+         let arr = []
+            for(let i = 0; i < $('.answer').length; i++){
+                let id = '#a' + i
+                arr[i] = '$("' + id + '").val()'
+              
             }
+            console.log(arr)
+         
+
+            // str =  $('#a0').val() 
+            //      + $('#a1').val() 
+            //      + $('#a2').val()
+            //      + $('#a3').val() 
+            //      + $('#a4').val()
+            //      + $('#a5').val()
+          
+              
+            // console.log(str)
+           
+     
+    if (str.length == ans.length && !str.includes(' ')) {
+        let result = str.toLowerCase()
+        if (result != ans) {
+            $('#level').addClass('wrong-answer')
+            $('.answer:first-child').focus()
+            $('#wrong-icon').show()
+
+            setTimeout(function () {
+                $('#level').removeClass('wrong-answer')
+            }, 1000);
+
+
+            let inputs = document.getElementsByClassName('answer')
+            for (let input of inputs) {
+                input.value = ''
+                str = ''
+            }
+            $('#level-text').css('color', 'red')
+            $('#wrong-icon').css('color', 'red')
+
+        } else {
+            $('#wrong-icon').hide()
+            $('#right-icon').show()
+            $('#wrong-icon').css('color', 'rgb(56, 250, 56)')
+            $('#level-text').css('color', 'rgb(56, 250, 56)')
+
+            $('#level').addClass('right-answer')
+            for (let i = 0; i < $(('.answer')).length; i++) {
+                $('.answer')[i].disabled = true
+            }
+            point += 10
+            console.log(point)
+            let poi = document.getElementById('point')
+            poi.innerHTML = `
+        <span> Diem: ${point}</span>
+        `
         }
-        })
-    })
+    }
 
+        })   
+    })
     
+   
 }
--- js/test.js
+++ js/test.js
@@ -1,33 +1,7 @@
-var left = document.getElementById("left");
-var right = document.getElementById("right");
-var text = document.getElementById("text");
 
-var messages = ["dog", "cat", "bear", "penguin", "tiger", "eagle", "John Doe"];
-
-    text.textContent = messages[0]
-
-
-    function next(current, messages) {
-        var idx = messages.indexOf(current);
-        if (idx === messages.length - 1) {
-          return messages[0];
-        }
-        return messages[idx + 1];
-      }
-      
-      function prev(current, messages) {
-        var idx = messages.indexOf(current);
-        if (idx === 0) {
-          return messages[messages.length - 1];
-        }
-        return messages[idx - 1];
-      }
-      left.addEventListener('click', () => {
-        text.textContent = prev(text.textContent, messages);
-      });
-      
-      right.addEventListener('click', () => {
-        text.textContent = next(text.textContent, messages);
-      });
-  
+  let idIn = 0 
+  for(let i = 0; i < 6; i++){
+     idIn= i
      
+  }
+  console.log(idIn)
